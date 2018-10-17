c9660f8f502f7a6e4e2f7579090d0b34de072fe8
Luke Strama <strama@ualberta.ca>
2018-10-17 10:02:19 -0600

    added wc operation

--- commands.js
+++ commands.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const readline = require('readline');
 
-function done(output) {
+done = (output) => {
     process.stdout.write(output);
     process.stdout.write('\nprompt > ');
 }
 
-function evaluateCmd(userInput) {
+evaluateCmd = (userInput) => {
     const userInputArray = userInput.split(" ");
     const command = userInputArray[0];
 
@@ -16,19 +17,52 @@ function evaluateCmd(userInput) {
         case "cat":
             commandLibrary.cat(userInputArray.slice(1));
             break;
+        case "wc": 
+            commandLibrary.wc(userInputArray.slice(1));
+        case "sort":
+            commandLibrary.sort(userInputArray.slice(1));
     }
 }
 
 const commandLibrary = {
-    echo: function(userInput) {
+    echo: (userInput) => {
         done(userInput);
     },
-    cat: function(fullPath) {
+    cat: (fullPath) => {
         const fileName = fullPath[0];
-        fs.readFile(fileName, (err, data) => {
+        fs.readFile(fileName, 'utf8', (err, data) => {
             if (err) throw err;
             done(data);
         });
+    },
+    wc: (fullPath) => {
+        const fileName = fullPath[0];
+        const fileSize = fs.statSync(fileName).size;
+        
+        try {
+            const words = fs.readFileSync(fileName, 'utf8').replace(/\r\n/g, " ").split(' ');
+            let wordCount = 0;
+            for (let i = 0; i < words.length; i++) {
+                if (words[i]) {
+                    wordCount++;
+                }
+            }
+            const lineReader = readline.createInterface({
+                input: fs.createReadStream(fileName),
+                crlfDelay: Infinity
+            });
+            let count = 0;
+            lineReader.on('line', (line) => {
+                count++;
+            }).on('close', function() {
+                done(`${count} ${wordCount} ${fileSize}`);
+            });
+        } catch (err) {
+            console.error(err);
+        }        
+    },
+    sort: (fullPath) => {
+        const fileName = fullPath[0];
     }
 };
 
